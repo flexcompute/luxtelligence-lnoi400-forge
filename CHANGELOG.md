5b8224100410c625522d3e39b0d8e4f81ed0f3f9
Lucas Heitzmann Gabrielli <lucas@flexcompute.com>
2025-03-04 14:09:57 -0300

    Release v1.2.0
    Signed-off-by: Lucas Heitzmann Gabrielli <lucas@flexcompute.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## 1.2.0 - Unreleased
+## 1.2.0 - 2025-03-04
 
 ### Added
 - Electrical ports and terminals.
--- luxtelligence_lnoi400_forge/__init__.py
+++ luxtelligence_lnoi400_forge/__init__.py
@@ -2,4 +2,4 @@ from .utils import place_edge_couplers
 from .technology import lnoi400
 from . import component
 
-__version__ = "1.2.0rc1"
+__version__ = "1.2.0"
